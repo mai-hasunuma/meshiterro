7eaac4dfd16e8b93ec8864be7e808a28db3733fe
蓮沼真愛 <hasumai@hasunumashinainoMacBook-Pro.local>
2019-08-07 18:29:47 +0900

    [Finish]5-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '808871b0e7a93b1fd3dcec782b9aa172ae91bac7867bb92426f77c20acf64e63db210a45cf239d96e6622515941e05809e100fba7f9f7989ef77c27d64231914'
